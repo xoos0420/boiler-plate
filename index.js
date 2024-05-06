b7b4ff104b85bca94805e5794f541194534023be
xoos0420 <xoos0420@gmail.com>
2024-05-06 23:54:38 +0900

    mkdir config

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
+const config = require('./config/key.js');
+
+const { User } = require('./models/user.js');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://xoos0420:<password>@boiler-plate.xxix3dh.mongodb.net/?retryWrites=true&w=majority&appName=boiler-plate', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+    .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-
-
 app.get('/', (req, res) => res.send('Hello World'));
+app.post('/register', async (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어줌
+    const user = new User(req.body);
 
-app.listen(port, () => console.log(`Example App listening on port ${port}`));
+    try {
+        const saveUser = await user.save();
+        res.status(200).json({
+            success: true, saveUser
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false, err
+        })
+    }
+    // const result = await user.save().then(()=>{
+    //     res.status(200).json({
+    //       success: true
+    //     })
+    //   }).catch((err)=>{
+    //     res.json({ success: false, err })
+    //   })
+});
+
+app.listen(port, () => console.log(`Example App listening on port ${port}!`));
